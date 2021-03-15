19d53fbe002ec6bf66b1630baca71c44c3cc4819
Berna Taşel <bernataselbal@gmail.com>
2021-03-15 14:26:02 +0300

    upgraded MainActivity.kt

--- app/src/main/java/com/ilaydaberna/imageprocessingbaseddietapp/ui/component/main/MainActivity.kt
+++ app/src/main/java/com/ilaydaberna/imageprocessingbaseddietapp/ui/component/main/MainActivity.kt
@@ -3,14 +3,16 @@ package com.ilaydaberna.imageprocessingbaseddietapp.ui.component.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ilaydaberna.imageprocessingbaseddietapp.R
+import com.ilaydaberna.imageprocessingbaseddietapp.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 
 
