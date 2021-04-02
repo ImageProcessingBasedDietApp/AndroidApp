ae519023b548d4f6a1f8043194750ca537983d6a
Berna Taşel <bernataselbal@gmail.com>
2021-04-02 11:30:25 +0300

    profileFragment's front side is upgraded.

--- app/src/main/java/com/ilaydaberna/imageprocessingbaseddietapp/ui/component/main/profile/ProfileFragment.kt
+++ app/src/main/java/com/ilaydaberna/imageprocessingbaseddietapp/ui/component/main/profile/ProfileFragment.kt
@@ -3,6 +3,7 @@ package com.ilaydaberna.imageprocessingbaseddietapp.ui.component.main.profile
 import android.app.Activity.RESULT_CANCELED
 import android.app.Activity.RESULT_OK
 import android.app.AlertDialog
+import android.app.DatePickerDialog
 import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.Bitmap
@@ -12,10 +13,16 @@ import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.widget.ViewUtils
 import androidx.fragment.app.Fragment
 import com.ilaydaberna.imageprocessingbaseddietapp.R
+import com.ilaydaberna.imageprocessingbaseddietapp.util.alertDialog
+import com.ilaydaberna.imageprocessingbaseddietapp.util.isEmpty
 import kotlinx.android.synthetic.main.fragment_profile.*
 import kotlinx.android.synthetic.main.fragment_profile.view.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class ProfileFragment : Fragment(){
@@ -24,7 +31,11 @@ class ProfileFragment : Fragment(){
     var isEditUserInfo = false
     var isEditSettings = false
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
         view.iv_profile_photo.setOnClickListener {
             selectImage()
@@ -70,9 +81,75 @@ class ProfileFragment : Fragment(){
         }
 
 
+        view.et_birthdate.setOnClickListener {
+            //TODO: DATEPİCKER EKLE
+
+        }
+
+        view.et_gender.setOnClickListener {
+            val options = arrayOf<CharSequence>( "Kadın", "Erkek", "Belirtmek İstemiyorum")
+            val builder: AlertDialog.Builder = AlertDialog.Builder(context)
+            builder.setItems(options, DialogInterface.OnClickListener { dialog, item ->
+                view.et_gender.setText(options[item].toString())
+            })
+            builder.show()
+        }
+
+        view.button_save_goals.setOnClickListener {
+            if(!view.et_goal_coffee.isEmpty() && !view.et_goal_weight.isEmpty() && !view.et_goal_water.isEmpty() && !view.et_goal_tea.isEmpty() && !view.et_goal_step.isEmpty()){
+                //TODO: firebase kaydet
+                view.tv_goal_weight.setText(view.et_goal_weight.text.toString() + " kg")
+                view.tv_goal_water.setText(view.et_goal_water.text.toString() + " bardak su")
+                view.tv_goal_coffee.setText(view.et_goal_coffee.text.toString() + " bardak Türk Kahvesi")
+                view.tv_goal_tea.setText(view.et_goal_tea.text.toString() + " bardak çay")
+                view.tv_goal_step.setText(view.et_goal_step.text.toString() + " adım")
+                view.layout_edit_goals.visibility = View.GONE
+                view.layout_goals.visibility = View.VISIBLE
+                isEditGoals = false
+            }
+            else{
+                context?.let { it1 -> this.alertDialog(it1, "Hedeflerimi Kaydet", "Lütfen tüm boşlukları doldurun", "Tamam", null, null) }
+            }
+
+        }
+
+        view.button_save_user_info.setOnClickListener {
+            if(!view.et_height.isEmpty() && !view.et_weight.isEmpty() && !view.et_birthdate.isEmpty() && !view.et_gender.isEmpty()){
+                //TODO: firebase kaydet
+                view.tv_height.setText(view.et_height.text.toString() + " cm")
+                view.tv_weight.setText(view.et_weight.text.toString() + " kg")
+                view.tv_birthdate.setText(view.et_birthdate.text.toString())
+                view.tv_gender.setText(view.et_gender.text.toString())
+                view.layout_user_info.visibility = View.VISIBLE
+                view.layout_edit_user_info.visibility = View.GONE
+                isEditUserInfo = false
+            }
+            else{
+                context?.let { it1 -> this.alertDialog(it1, "Bilgilerimi Kaydet", "Lütfen tüm boşlukları doldurun", "Tamam", null, null) }
+            }
+
+        }
+
+        view.button_save_settings.setOnClickListener {
+            //TODO: firebase kaydet
+            if(sw_notification.isChecked){
+                view.tv_notification.setText("Bildirimler açık")
+            }
+            else{
+                view.tv_notification.setText("Bildirimler kapalı")
+            }
+            view.layout_settings.visibility = View.VISIBLE
+            view.layout_edit_settings.visibility = View.GONE
+            isEditSettings = false
+        }
+
+
         return view
     }
 
+    fun initViews(view: View){
+        //TODO: Firebase'den gelenler ile doldur
+    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (resultCode != RESULT_CANCELED) {
--- app/src/main/java/com/ilaydaberna/imageprocessingbaseddietapp/util/ViewUtils.kt
+++ app/src/main/java/com/ilaydaberna/imageprocessingbaseddietapp/util/ViewUtils.kt
@@ -1,10 +1,16 @@
 package com.ilaydaberna.imageprocessingbaseddietapp.util
 
+import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+import com.google.firebase.firestore.WriteBatch
 import com.ilaydaberna.imageprocessingbaseddietapp.ui.component.camera.CameraActivity
 import com.ilaydaberna.imageprocessingbaseddietapp.ui.component.login.LoginActivity
 import com.ilaydaberna.imageprocessingbaseddietapp.ui.component.main.MainActivity
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 fun Context.startHomeActivity() =
@@ -24,3 +30,26 @@ fun Context.startCameraActivity() =
         it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         startActivity(it)
     }
+
+fun EditText.isEmpty() = this.text.isEmpty()
+
+fun Fragment.alertDialog(context: Context, title: String, msg: String, positiveBtn: String, negativeBtn: String?, positive: WriteBatch.Function?){
+    val builder = AlertDialog.Builder(context)
+    builder.setTitle(title)
+    builder.setMessage(msg)
+
+    builder.setPositiveButton(android.R.string.yes) { dialog, which ->
+        if(positive == null){
+            dialog.dismiss()
+        }
+    }
+
+    if(negativeBtn != null){
+        builder.setNegativeButton(android.R.string.no) { dialog, which ->
+            dialog.dismiss()
+        }
+    }
+
+    builder.show()
+
+}
